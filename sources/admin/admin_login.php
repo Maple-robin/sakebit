85a6a290b097c1a856526f22bc2c1c96daf0c9f7
maruaki082 <mingwanmucun720@i.softbank.jp>
2025-07-15 12:02:11 +0900

    ログインのヘッダー直した、ヘッダーが場所によって結構位置がずれるから調整したほうがいい

--- sources/admin/admin_login.php
+++ sources/admin/admin_login.php
@@ -196,11 +196,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <ul class="admin-nav__list">
                     <li><a href="admin_products.php">お酒管理</a></li>
                     <li><a href="admin_otsumami.php">おつまみ管理</a></li>
-                    <li><a href="admin_users.php">ユーザー管理</a></li>
+                    <li><a href="admin_users.php">一般ユーザー管理</a></li>
+                    <li><a href="admin_client_users.php">企業ユーザー管理</a></li>
                     <li><a href="admin_posts.php">投稿管理</a></li>
                     <li><a href="admin_inquiries.php">お問い合わせ管理</a></li>
                     <li><a href="admin_faq.php">FAQ管理</a></li>
                     <li><a href="admin_reports.php">通報管理</a></li>
+                    <li><a href="admin_login.php" class="is-current" tabindex="-1" style="pointer-events: none;">ログイン</a></li>
                 </ul>
             </nav>
         </div>
